7e325070302a22b6ec365afb47146601fe3aa40c
Henri Koski <henri.t.koski@gmail.com>
2014-11-08 11:31:06 +0200

    merge from denzil

--- lux_meter/build/generated/source/r/debug/com/aware/lib/achartandroid/R.java
+++ lux_meter/build/generated/source/r/debug/com/aware/lib/achartandroid/R.java
@@ -8,7 +8,7 @@ package com.aware.lib.achartandroid;
 
 public final class R {
 	public static final class drawable {
-		public static final int ic_launcher = 0x7f020061;
+		public static final int ic_launcher = 0x7f020060;
 	}
 	public static final class string {
 		public static final int app_name = 0x7f0d000d;
